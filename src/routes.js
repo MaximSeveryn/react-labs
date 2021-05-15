8d83b9162b7043c7a310b14918e3c1c7d7c4f8ef
Miroslav Bushchuk <21muros2001@gmail.com>
2021-05-15 16:18:30 +0300

    has 2 pages

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Navigate } from 'react-router-dom';
+import DashboardLayout from 'src/components/DashboardLayout';
+import CustomerList from 'src/pages/CustomerList';
+import ProductList from 'src/pages/ProductList';
+
+const routes = [
+  {
+    path: '/',
+    element: <DashboardLayout />,
+    children: [
+      { path: '/', element: <Navigate to="/customers" /> },
+      { path: 'customers', element: <CustomerList /> },
+      { path: 'products', element: <ProductList /> },
+    ]
+  },
+
+];
+
+export default routes;
