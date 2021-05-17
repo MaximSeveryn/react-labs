2dd565ff2d94ffe070a89d614a9a4c47495842c7
Miroslav Bushchuk <21muros2001@gmail.com>
2021-05-17 12:02:00 +0300

    all parts done

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -1,23 +1,28 @@
 import { useEffect } from 'react';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {
-  Avatar,
-  Box,
-  Drawer,
-  Hidden,
-  Typography
-} from '@material-ui/core';
+import { Avatar, Box, Drawer, Hidden, Typography } from '@material-ui/core';
 import {
   ShoppingBag as ShoppingBagIcon,
   Users as UsersIcon
 } from 'react-feather';
 import NavItem from './NavItem';
+import React, { useState } from 'react';
 
-const user = {
-  avatar: '/static/images/avatars/avatar_6.png',
-  jobTitle: 'Senior Developer',
-  name: 'Katarina Smith'
+const getData = async (url) => {
+  const res = await fetch(url);
+  if (res.ok) {
+    return res.json();
+  }
+  return 0;
+};
+
+const getUser = () => {
+  const userName = 'MaximSeveryn'.toLowerCase();
+  const url = `https://api.github.com/users/${userName}`;
+  return getData(url).then((response) => {
+    return [response.avatar_url, response.login];
+  });
 };
 
 const items = [
@@ -35,6 +40,9 @@ const items = [
 
 const DashboardSidebar = ({ onMobileClose, openMobile }) => {
   const location = useLocation();
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [userItems, setItems] = useState([]);
 
   useEffect(() => {
     if (openMobile && onMobileClose) {
@@ -42,83 +50,98 @@ const DashboardSidebar = ({ onMobileClose, openMobile }) => {
     }
   }, [location.pathname]);
 
-  const content = (
-    <Box
-      className="sidebar__wrapper"
-    >
-      <Box
-        className="sidebar__content"
-      >
-        <Avatar
-          className="user-avatar"
-          component={RouterLink}
-          src={user.avatar}
-          sx={{
-            cursor: 'pointer',
-            width: 64,
-            height: 64
-          }}
-          to="/"
-        />
-        <Typography color="textPrimary" variant="h5">
-          {user.name}
-        </Typography>
-        <Typography color="textSecondary" variant="body2">
-          {user.jobTitle}
-        </Typography>
-      </Box>
-      <hr className="sidebar__divider divider" />
-      <Box className="sidebar__content" sx={{ p: 2 }}>
-        <ul className="nav-list sidebar__nav-list">
-          {items.map((item) => (
-            <NavItem
-              href={item.href}
-              key={item.title}
-              title={item.title}
-              icon={item.icon}
-            />
-          ))}
-        </ul>
-      </Box>
-      <div className="sidebar__spacing spacing" />
-    </Box>
-  );
+  useEffect(() => {
+    getUser().then(
+      (result) => {
+        setItems(result);
+        setIsLoaded(true);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  }, []);
 
-  return (
-    <>
-      <Hidden lgUp>
-        <Drawer
-          anchor="left"
-          onClose={onMobileClose}
-          open={openMobile}
-          variant="temporary"
-          PaperProps={{
-            sx: {
-              width: 256
-            }
-          }}
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-      <Hidden lgDown>
-        <Drawer
-          anchor="left"
-          open
-          variant="persistent"
-          PaperProps={{
-            sx: {
-              width: 256,
-              top: 64,
-              height: 'calc(100% - 64px)'
-            }
-          }}
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-    </>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    const content = (
+      <Box className="sidebar__wrapper">
+        <Box className="sidebar__content">
+          <Avatar
+            className="user-avatar"
+            component={RouterLink}
+            src={userItems[0]}
+            sx={{
+              cursor: 'pointer',
+              width: 64,
+              height: 64
+            }}
+            to="/"
+          />
+          <Typography color="textPrimary" variant="h5">
+            {userItems[1]}
+          </Typography>
+          <Typography color="textSecondary" variant="body2">
+            Stydent
+          </Typography>
+        </Box>
+        <hr className="sidebar__divider divider" />
+        <Box className="sidebar__content" sx={{ p: 2 }}>
+          <ul className="nav-list sidebar__nav-list">
+            {items.map((item) => (
+              <NavItem
+                href={item.href}
+                key={item.title}
+                title={item.title}
+                icon={item.icon}
+              />
+            ))}
+          </ul>
+        </Box>
+        <div className="sidebar__spacing spacing" />
+      </Box>
+    );
+
+    return (
+      <>
+        <Hidden lgUp>
+          <Drawer
+            anchor="left"
+            onClose={onMobileClose}
+            open={openMobile}
+            variant="temporary"
+            PaperProps={{
+              sx: {
+                width: 256
+              }
+            }}
+          >
+            {content}
+          </Drawer>
+        </Hidden>
+        <Hidden lgDown>
+          <Drawer
+            anchor="left"
+            open
+            variant="persistent"
+            PaperProps={{
+              sx: {
+                width: 256,
+                top: 64,
+                height: 'calc(100% - 64px)'
+              }
+            }}
+          >
+            {content}
+          </Drawer>
+        </Hidden>
+      </>
+    );
+  }
 };
 
 DashboardSidebar.propTypes = {
